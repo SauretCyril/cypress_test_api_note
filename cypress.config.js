f9eaa819a05784419f55ec181cee8ce49e21d375
SAURET Cyril <sauretcyril@outlook.fr>
2024-07-05 13:42:34 +0200

    add report

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,13 @@ const { defineConfig } = require("cypress");
 const { downloadFile } = require("cypress-downloadfile/lib/addPlugin");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
       on("task", { downloadFile });
+    },
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
   env: {
